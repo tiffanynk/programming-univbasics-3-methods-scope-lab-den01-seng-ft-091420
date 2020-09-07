2de3363a255324f0eadf98f952898249b808fbe3
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-07 17:49:49 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,4 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = 'It\'s-a me, Mario!'
   puts phrase
 end
@@ -7,5 +6,6 @@ end
 mario
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
